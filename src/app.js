e527755d440df9587e3a171a29f22422d30148a1
Alireza Akbarzadeh <82927248+alireza-akbarzadeh@users.noreply.github.com>
2025-10-07 10:56:16 +0330

    Add development dependencies for ESLint and Prettier

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
-import express from 'express'
+import express from 'express';
 
 const app = express();
 
-
 app.get('/', (req, res) => {
-    res.status(200).send('Hello World Acquisitions!')
-})
+  res.status(200).send('Hello World Acquisitions!');
+});
 
 export default app;
